e3c1daf3c4afd0d7bfa410d66598498030a45fe1
Waqar Ali <waqar.ali911@gmail.com>
2016-08-22 01:55:11 +0400

    Url from input field and url validation with api call.

--- app/app.controller.js
+++ app/app.controller.js
@@ -2,14 +2,43 @@ import X2JS from 'x2js';
 
 class AppController {
   constructor($http) {
-    this.ctrl = 'appCtrl';
+    let defaultUrl = 'http://pf.tradetracker.net/?aid=1&type=xml&encoding=utf-8&fid=251713&categoryType=2&additionalType=2&limit=10';
 
-    $http
-      .get('https://crossorigin.me/http://pf.tradetracker.net/?aid=1&type=xml&encoding=utf-8&fid=251713&categoryType=2&additionalType=2&limit=10')
-      .then(function(res) {
+    this.$http = $http;
+    this.urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+    this.showError = false;
+    this.feedUrl = defaultUrl;
+    this.feeds = [];
+
+    this.loadFeeds(defaultUrl);
+  }
+
+  requestFeed(formFields) {
+    if (formFields.$invalid) {
+      this.showError = true;
+      return;
+    }
+
+    this.showError = false;
+    loadFeeds(formFields.$viewValue);
+  }
+
+  loadFeeds(url) {
+    this.requestService(url)
+      .then(res => {
+        this.feeds = res.products.product;
+      });
+  }
+
+  requestService(url) {
+    const CORS_PROXY = 'https://crossorigin.me/';
+
+    return this.$http
+      .get(CORS_PROXY + url)
+      .then(res => {
         let x2js = new X2JS();
         let jsonOut = x2js.xml2js(res.data);
-        console.log(jsonOut);
+        return jsonOut;
       });
   }
 }
